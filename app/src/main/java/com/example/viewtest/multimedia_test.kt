ddca021b7e2c2dbdc3cd956184b779eea2c318b4
felix <1306332027@qq.com>
2021-07-06 15:48:11 +0800

    "增加了视频播放"

--- app/src/main/java/com/example/viewtest/multimedia_test.kt
+++ app/src/main/java/com/example/viewtest/multimedia_test.kt
@@ -1,8 +1,10 @@
 package com.example.viewtest
 
 import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.VideoView
 
 class multimedia_test : BaseActivity() {
     val mediaPlayer=MediaPlayer()
@@ -35,10 +37,30 @@ class multimedia_test : BaseActivity() {
                 mediaPlayer.prepare()
             }
         }
+        val uri= Uri.parse("android.resource://$packageName/${R.raw.video}")
+        val videoView=findViewById<VideoView>(R.id.videoView)
+        videoView.setVideoURI(uri)
+        val play_vedio=findViewById<Button>(R.id.play_video)
+        play_vedio.setOnClickListener {
+           if(!videoView.isPlaying){
+               videoView.start()
+           }
+        }
+        val pause_video=findViewById<Button>(R.id.pause_video)
+        pause_video.setOnClickListener {
+            if(videoView.isPlaying){
+                videoView.pause()
+            }
+        }
+        val replay_video=findViewById<Button>(R.id.replay_video)
+        replay_video.setOnClickListener {
+            videoView.resume()
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        findViewById<VideoView>(R.id.videoView).suspend()
         mediaPlayer.stop()
         mediaPlayer.release()
     }
